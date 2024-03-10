6723d49d29f6c87927aaca0865c0c48a8e33fcd3
fakePetr0vi4 <kotkotkot034+github@gmail.com>
2024-03-10 22:23:16 +0300

    Initial commit

---
+++ src/main/java/Plant.java
@@ -0,0 +1,9 @@
+class Plant {
+    String name;
+    public Plant(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+     class main {
+        public static String filter(Plant plant) {
+            String filteredName = plant.getName().replaceAll("[AEIOUYaeiouаеиоуыэюяАЕИОУЫЭЮЯ]", "");
+            filteredName = filteredName + "VGTBL";
+            return filteredName;
+        }
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите название растения на английском или русском языке:");
+            String plantName = scanner.nextLine();
+            Plant plant = new Plant(plantName);
+            String filteredName = filter(plant);
+            System.out.println(plantName + ", "  + filteredName);
+        }
+    }
+
+
